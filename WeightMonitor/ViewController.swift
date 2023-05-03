850e71eaca12649cada5d75a2ba4450649447775
filosuf <filosuf@mail.ru>
2023-05-03 15:19:22 +0300

    Initial Commit

---
+++ WeightMonitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeightMonitor
+//
+//  Created by Filosuf on 03.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
