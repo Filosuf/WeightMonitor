658bd8d12aa38cd6e9ea8532e5bbcc4f35464673
filosuf <filosuf@mail.ru>
2023-05-04 09:14:42 +0300

    added colors in assets and UIColor extension

---
+++ WeightMonitor/Utils/Extensions/UIColor+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+Extension.swift
+//  WeightMonitor
+//
+//  Created by Filosuf on 04.05.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    enum Custom {
+        static var black: UIColor? { UIColor(named: "black") }
+        static var blueButton: UIColor? { UIColor(named: "blueButton") }
+        static var blueButtonDayDark: UIColor? { UIColor(named: "blueButtonDayDark") }
+        static var grayBackground: UIColor? { UIColor(named: "grayBackground") }
+        static var grayText: UIColor? { UIColor(named: "grayText") }
+        static var lightGrayText: UIColor? { UIColor(named: "lightGrayText") }
+        static var whiteTextTost: UIColor? { UIColor(named: "whiteTextTost") }
+    }
+}
